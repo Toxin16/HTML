083117c6a98d32f73ea622c20b0a5d7eaa4257ac
Toxin16 <109052012+Toxin16@users.noreply.github.com>
2022-07-19 12:49:50 +0900

    Add files via upload

---
+++ JS_Begin/num.js
@@ -0,0 +1,11 @@
+let oct = 015;
+let hex = 0x15;
+let codition = true;
+
+document.write("8진수 015는 십진수로" + oct +"<br>");
+document.write("16진수 0x15는 십진수로" + hex +"<br>");
+document.write("condition은" +  codition +"<br>");
+document.write('문자열 : 단일인용부호로도 표현' + "<br>");
+document.write("그녀는 \"누구세요\"라고 했습니다.");
+document.write("줄바꿈은<br>새로운 줄에 출력<br>");
+document.write("탭을 3번 입력했습니다.");
