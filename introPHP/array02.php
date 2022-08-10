0730303d6bef93f6c33dbd48d1da9df11c54d71b
Toxin16 <109052012+Toxin16@users.noreply.github.com>
2022-08-10 11:56:16 +0900

    Add files via upload

---
+++ introPHP/array02.php
@@ -0,0 +1,7 @@
+<?php
+$multtype = array(20, 3.14, 'A', "Bob");
+print "$multtype[0] <br>";
+print "$multtype[1] <br>";
+print "$multtype[2] <br>";
+print "$multtype[3] <br>";
+?>
---
+++ introPHP/for01.php
@@ -0,0 +1,17 @@
+<?PHP
+$i = 1;
+$sum = 0;
+for($i=1; $i<=100; $i++) {
+    $sum += $i;
+}
+print"sum = " . $sum;
+
+# while
+$i = 1;
+$sum = 0;
+while($i <= 100) {
+    $sum += $i;
+    $i++;
+}
+print("sum = " . $sum );
+?>
